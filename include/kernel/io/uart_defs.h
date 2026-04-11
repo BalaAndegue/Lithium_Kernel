f71d37541c158680b8beab8c0327996eec35ba23
BalaAndegue <balaandeguefrancoislionnel@gmail.com>
2026-04-11 17:16:06 +0100

    Add UART definitions header (placeholder for advanced defs)

---
+++ include/kernel/io/uart_defs.h
@@ -0,0 +1,8 @@
+
+#ifndef KERNEL_IO_UART_DEFS_H
+#define KERNEL_IO_UART_DEFS_H
+
+// Ce fichier contient les définitions avancées pour l'UART
+// Actuellement minimal, sera complété plus tard
+
+#endif
